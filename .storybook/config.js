7c5253a273fa5d9704765f6c38bb47a7d338e484
Nicolas Fournier <nicolas.fournier.2@umontreal.ca>
2019-04-26 09:30:26 -0400

    JTableRow component

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/vue';
 // automatically import all files ending in *.stories.js
 const req = require.context(
-  '../components',
+  '../components/',
   true,
   /\.stories\.js$/
 );
--- components/JTableRow/JTableRow.vue
+++ components/JTableRow/JTableRow.vue
@@ -7,24 +7,24 @@
 </template>
 
 <script>
-export default {
-  name: 'JTableRow',
-  props: {
-    values: {
-      type: Array,
-      required: true
-    }
-  }
-}
+	export default {
+	name: 'JTableRow',
+	props: {
+		values: {
+		type: Array,
+		required: true
+		}
+	}
+	}
 </script>
 
 <style scoped>
-tr {
-  background-color: white;
-  border-radius: 4px;
-  box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.08);
-}
-td {
-  padding: 8px;
-}
+	tr {
+	background-color: white;
+	border-radius: 4px;
+	box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.08);
+	}
+	td {
+	padding: 8px;
+	}
 </style>
